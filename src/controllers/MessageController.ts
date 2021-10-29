ac28006671a87ee5302235c49b43b89df1b94795
Jean Fraga <jeanfragoli395@gmail.com>
2021-10-29 09:21:41 -0300

    controller de message criado

---
+++ src/controllers/MessageController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express"
+import {MessageServices} from "../services/MessageServices"
+
+class MessageController {
+
+  async create (req: Request, res: Response) {
+    const {admin_id, text, user_id} = req.body
+    const messageServices = new MessageServices();
+
+    const message =  messageServices.create({
+      admin_id,
+      text,
+      user_id
+    })
+
+    return res.json(message)
+  }
+}
+
+export { MessageController}
--- src/services/MessageServices.ts
+++ src/services/MessageServices.ts
@@ -2,7 +2,7 @@ import {getCustomRepository} from "typeorm"
 import  { MessagesRepository } from "../repositories/MessagesRepository"
 
 interface IMessageCreate {
-  admin_id: string,
+  admin_id?: string,
   text: string,
   user_id: string
 }
