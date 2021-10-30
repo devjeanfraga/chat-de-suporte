96de2667bf8cccb0f160584c72fcab218f89426b
Jean Fraga <jeanfragoli395@gmail.com>
2021-10-30 05:31:19 -0300

    emit do websocket implementado

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,4 +7,26 @@ let socket = null;
 
 document.querySelector("#start_chat").addEventListener("click", (event) => {
   const socket = io();
-});
+
+  const chat_help = document.getElementById("chat_help");
+  chat_help.style.display = "none";
+
+  const chat_in_support = document.getElementById("chat_in_support");
+  chat_in_support.style.display = "block";
+
+  const email = document.getElementById('email').value;
+  const text = document.getElementById('txt_help').value;
+
+  //Emissão de eventos com websockets
+  socket.on("connect", () => {
+    const params = { email, text } 
+    socket.emit("client_first_access", params, (call, err) => {
+      if(err) {
+        console.log(err);
+      } else  {
+        console.log(call);
+      }
+    });
+  });
+
+}); //fim querySelector
