86fed2423d806f1a67597e14949f8a89805d7eb7
Jean Fraga <jeanfragoli395@gmail.com>
2021-11-02 09:13:03 -0300

    rota para habilitar botão de cha, boolean

--- routes.ts
+++ routes.ts
@@ -10,6 +10,8 @@ const routes = Router()
 
 
 routes.post('/settings', settingsController.create);
+routes.get('/settings/:username', settingsController.findByUserName)
+routes.put('/settings/:username', settingsController.aupdate)
 routes.post('/users', usersController.create);
 routes.post ('/messages', messageController.create);
 routes.get('/messages/:id', messageController.showByUser)
